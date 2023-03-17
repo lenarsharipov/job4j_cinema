00a879ed5044c4a1d61963b32a1b039f713dbdf5
lenarsharipov <lenarsharipov@gmail.com>
2023-03-17 21:47:30 +0300

    6. Добавление скрипта 006_dml_insert_film_sessions.sql

---
+++ db/scripts/006_dml_insert_film_sessions.sql
@@ -0,0 +1,136 @@
+/* Сеансы на фильм "На солнце, вдоль рядов кукурузы" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 4, '2023-04-01 10:20', '2023-04-01 11:50', 210);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 3, '2023-04-01 11:20', '2023-04-01 12:50', 410);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 4, '2023-04-01 12:40', '2023-04-01 14:10', 250);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 3, '2023-04-01 13:30', '2023-04-01 15:00', 450);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 4, '2023-04-01 14:50', '2023-04-01 16:20', 270);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 4, '2023-04-01 17:00', '2023-04-01 18:30', 270);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 3, '2023-04-01 17:50', '2023-04-01 19:20', 570);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 4, '2023-04-01 19:10', '2023-04-01 20:40', 340);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 3, '2023-04-01 20:00', '2023-04-01 21:30', 570);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 4, '2023-04-01 21:30', '2023-04-01 23:00', 340);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(1, 4, '2023-04-01 23:45', '2023-04-02 01:15', 340);
+
+/* Сеансы на фильм "Человек-муравей и Оса: Квантомания" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(2, 2, '2023-04-01 10:10', '2023-04-01 12:15', 250);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(2, 2, '2023-04-01 12:50', '2023-04-01 14:55', 290);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(2, 2, '2023-04-01 15:30', '2023-04-01 17:35', 310);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(2, 2, '2023-04-01 18:10', '2023-04-01 20:15', 380);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(2, 2, '2023-04-01 20:50', '2023-04-01 22:55', 380);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(2, 2, '2023-04-01 23:30', '2023-04-02 01:35', 310);
+
+/* Сеансы на фильм "Поехавшая" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(3, 5, '2023-04-01 10:35', '2023-04-01 11:51', 180);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(3, 6, '2023-04-01 14:00', '2023-04-01 15:16', 180);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(3, 6, '2023-04-01 16:40', '2023-04-01 17:56', 180);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(3, 6, '2023-04-01 19:20', '2023-04-01 20:36', 180);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(3, 3, '2023-04-01 22:10', '2023-04-01 23:26', 300);
+
+/* Сеансы на фильм "Праведник" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(4, 5, '2023-04-01 13:10', '2023-04-01 15:50', 180);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(4, 5, '2023-04-01 16:30', '2023-04-01 19:10', 180);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(4, 5, '2023-04-01 19:50', '2023-04-01 22:30', 180);
+
+/* Сеансы на фильм "Нюрнберг" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(5, 7, '2023-04-01 10:40', '2023-04-01 12:55', 180);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(5, 7, '2023-04-01 15:50', '2023-04-01 18:05', 180);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(5, 7, '2023-04-01 18:30', '2023-04-01 20:45', 180);
+
+/* Сеансы на фильм "Аватар: Путь воды" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(6, 1, '2023-04-01 15:10', '2023-04-01 18:36', 270);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(6, 1, '2023-04-01 19:00', '2023-04-01 22:26', 340);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(6, 1, '2023-04-01 22:50', '2023-04-02 02:16', 270);
+
+/* Сеансы на фильм "Беспринципные в деревне" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(7, 3, '2023-04-01 15:40', '2023-04-01 17:20', 300);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(7, 7, '2023-04-01 21:20', '2023-04-01 23:00', 180);
+
+/* Сеансы на фильм "Кот в сапогах: Последнее желание" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(8, 1, '2023-04-01 10:30', '2023-04-01 12:14', 210);
+
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(8, 1, '2023-04-01 12:50', '2023-04-01 14:34', 250);
+
+/* Сеансы на фильм "Чебурашка" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(9, 7, '2023-04-01 13:20', '2023-04-01 15:20', 180);
+
+/* Сеансы на фильм "Царевны и таинственная гостья" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(10, 6, '2023-04-01 10:15', '2023-04-01 11:27', 180);
+
+/* Сеансы на фильм "Эскортница" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(11, 6, '2023-04-01 21:50', '2023-04-01 23:20', 180);
+
+/* Сеансы на фильм "Сергий против нечисти. Шабаш" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(12, 7, '2023-04-01 23:40', '2023-04-02 01:20', 180);
+
+/* Сеансы на фильм "Дети кукурузы" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(13, 5, '2023-04-01 23:10', '2023-04-02 00:43', 180);
+
+/* Сеансы на фильм "Стражи времени" */
+insert into film_sessions(film_id, halls_id, start_time, end_time, price)
+values(14, 6, '2023-04-01 11:50', '2023-04-01 13:39', 180);
