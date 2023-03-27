116f23891f3cca6e1f69ed8db17c293a31d1614e
lenarsharipov <lenarsharipov@gmail.com>
2023-03-27 15:03:11 +0300

    9. Добавление моделей - File, Film, Genre, Hall, Session, Ticket, User

---
+++ src/main/java/ru/job4j/cinema/model/Session.java
@@ -0,0 +1,98 @@
+package ru.job4j.cinema.model;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.Objects;
+
+public class Session {
+    public static final Map<String, String> COLUMN_MAPPING = Map.of(
+            "id", "id",
+            "film_id", "filmId",
+            "halls_id", "hallId",
+            "start_time", "startTime",
+            "end_time", "endTime",
+            "price", "price"
+    );
+
+    private int id;
+    private int filmId;
+    private int hallId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private int price;
+
+    public Session(int id, int filmId, int hallId, LocalDateTime startTime,
+                   LocalDateTime endTime, int price) {
+        this.id = id;
+        this.filmId = filmId;
+        this.hallId = hallId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFilmId() {
+        return filmId;
+    }
+
+    public void setFilmId(int filmId) {
+        this.filmId = filmId;
+    }
+
+    public int getHallId() {
+        return hallId;
+    }
+
+    public void setHallId(int hallId) {
+        this.hallId = hallId;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Session that = (Session) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
